32084035db7d0fb9317ebc63711318113c7fc463
Rony163 <minhajulislamrony43@gmail.com>
2021-10-29 03:26:36 +0600

    database connected

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middlewire
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gltfs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('database connected')
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running BD Travel Agency Server');
+});
+
+app.listen(port, () => {
+    console.log('Server running on port:', port);
+});
